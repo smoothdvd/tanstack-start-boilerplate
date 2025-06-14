a96794135bf6de5c5fc571a108fb527aa7e4385e
smoothdvd <madfxgao@gmail.com>
2025-06-14 10:52:42 +0800

    feat: enable DefaultCatchBoundary as default error component

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,14 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router';
 
-import { NotFound } from '@/components/NotFound';
-
+import { DefaultCatchBoundary } from './components/DefaultCatchBoundary';
+import { NotFound } from './components/NotFound';
 import { routeTree } from './routeTree.gen';
 
 export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
     defaultPreload: 'intent',
-    // defaultErrorComponent: DefaultCatchBoundary,
+    defaultErrorComponent: DefaultCatchBoundary,
     defaultNotFoundComponent: () => <NotFound />,
     scrollRestoration: true,
   });
